220701b23471e2ad3bc1faa1d3e0c68d045ca96e
satwinder6510 <142193859+satwinder6510@users.noreply.github.com>
2025-04-23 15:51:18 +0100

    last try

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    // only for `npm run dev` locally
     proxy: {
       '/api': 'http://localhost:4000'
     }
